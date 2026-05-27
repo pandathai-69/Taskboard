ac376985f13b855a8210b7252f6f346552c910c5
pandathai-69 <dancch714@gmail.com>
2026-05-27 15:49:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-var CACHE = 'taskboard-v1';
-var FILES = ['/', '/index.html'];
+var CACHE = 'taskboard-v2';
+var FILES = [
+  '/taskboard/index.html',
+  '/taskboard/manifest.json',
+  '/taskboard/icon.svg'
+];
 
+// Install: cache all files immediately
 self.addEventListener('install', function(e) {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(function(c) {
       return c.addAll(FILES);
@@ -9,10 +15,31 @@ self.addEventListener('install', function(e) {
   );
 });
 
+// Activate: clear old caches immediately
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+// Fetch: serve from cache first, fallback to network
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
-      return r || fetch(e.request);
+      return r || fetch(e.request).then(function(res) {
+        var clone = res.clone();
+        caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+        return res;
+      });
+    }).catch(function() {
+      return caches.match('/taskboard/index.html');
     })
   );
 });
